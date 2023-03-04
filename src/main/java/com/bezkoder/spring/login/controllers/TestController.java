0da7f415aab967b88ea8e03c9b1044c53b89a647
ftias <ftiastiti@gmail.com>
2023-03-05 06:47:44 +0700

    commit

--- src/main/java/com/bezkoder/spring/login/controllers/TestController.java
+++ src/main/java/com/bezkoder/spring/login/controllers/TestController.java
@@ -5,6 +5,10 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Arrays;
+import java.util.List;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
@@ -13,6 +17,15 @@ public class TestController {
   @GetMapping("/all")
   public String allAccess() {
     return "Public Content.";
+  }
+
+
+  @GetMapping(value = "/callDans")
+  public List<Object> getCall(){
+    String url = "http://dev3.dansmultipro.co.id/api/recruitment/positions.json";
+    RestTemplate restTemplate = new RestTemplate();
+    Object[] result = restTemplate.getForObject(url,Object[].class);
+    return Arrays.asList(result);
   }
 
   @GetMapping("/user")
